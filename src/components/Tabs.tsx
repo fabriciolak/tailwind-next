7a5281fae7c7cff737ae49a66f707e628ac16a00
Fabricio <fabriciolak12@gmail.com>
2023-08-04 09:45:28 -0300

    feat: tabs animation using framer motion

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as RadixTabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 interface ItemProps {
   value: string
@@ -17,7 +18,10 @@ export function Item({ value, title, isSelected = false }: ItemProps) {
       <span>{title}</span>
 
       {isSelected && (
-        <div className="absolute -bottom-px left-0 right-0 h-0.5 rounded-full bg-violet-700"></div>
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 rounded-full bg-violet-700"
+        ></motion.div>
       )}
     </RadixTabs.Trigger>
   )
