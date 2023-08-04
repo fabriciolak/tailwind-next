1dbe2de787137839405ed54c1d9a36170362801f
Fabricio <fabriciolak12@gmail.com>
2023-08-04 09:29:00 -0300

    feat: tabs using radix-ui tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,58 @@
+'use client'
+
+import * as React from 'react'
+import * as Tabs from '@/components/Tabs'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  const [currentTab, setCurrentTab] = React.useState<string>('my-details')
+
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
+        <Tabs.Item
+          value="my-details"
+          title="My Details"
+          isSelected={currentTab === 'my-details'}
+        />
+        <Tabs.Item
+          value="profile"
+          title="Profile"
+          isSelected={currentTab === 'profile'}
+        />
+        <Tabs.Item
+          value="password"
+          title="Password"
+          isSelected={currentTab === 'password'}
+        />
+        <Tabs.Item value="eam" title="Team" isSelected={currentTab === 'eam'} />
+        <Tabs.Item
+          value="plan"
+          title="Plan"
+          isSelected={currentTab === 'plan'}
+        />
+        <Tabs.Item
+          value="billing"
+          title="Billing"
+          isSelected={currentTab === 'billing'}
+        />
+        <Tabs.Item
+          value="email"
+          title="Email"
+          isSelected={currentTab === 'email'}
+        />
+        <Tabs.Item
+          value="notifications"
+          title="Notifications"
+          isSelected={currentTab === 'notifications'}
+        />
+        <Tabs.Item
+          value="integrations"
+          title="Integrations"
+          isSelected={currentTab === 'integrations'}
+        />
+        <Tabs.Item value="api" title="API" isSelected={currentTab === 'api'} />
+      </Tabs.Root>
+    </>
+  )
 }
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import * as RadixTabs from '@radix-ui/react-tabs'
+
+interface ItemProps {
+  value: string
+  title: string
+  isSelected?: boolean
+}
+
+export function Item({ value, title, isSelected = false }: ItemProps) {
+  return (
+    <RadixTabs.Trigger
+      value={value}
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state='active']:text-violet-700"
+    >
+      <span>{title}</span>
+
+      {isSelected && (
+        <div className="absolute -bottom-px left-0 right-0 h-0.5 rounded-full bg-violet-700"></div>
+      )}
+    </RadixTabs.Trigger>
+  )
+}
+
+type RootProps = React.RefAttributes<HTMLDivElement> & RadixTabs.TabsProps
+
+export function Root({ ...props }: RootProps) {
+  return (
+    <RadixTabs.Root {...props}>
+      <RadixTabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+        {props.children}
+      </RadixTabs.List>
+    </RadixTabs.Root>
+  )
+}
