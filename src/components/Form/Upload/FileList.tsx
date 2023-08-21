3790729899749a3410929084e1e3920162c56ff3
Fabricio <fabriciolak12@gmail.com>
2023-08-21 10:58:46 -0300

    feat: animations and accessibility

--- src/components/Form/Upload/FileList.tsx
+++ src/components/Form/Upload/FileList.tsx
@@ -3,12 +3,14 @@
 import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 import { formatBytes } from '@/utilts/format-byes'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ type ControlProps = React.ComponentProps<'input'>
 export function Control(props: ControlProps) {
   return (
     <input
-      className="h-full w-full border-0 bg-transparent p-0 text-zinc-900 outline-0 placeholder:text-zinc-600"
+      className="h-full w-full border-0 bg-transparent p-0 text-zinc-900 outline-none placeholder:text-zinc-600"
       placeholder="search"
       {...props}
     />
@@ -21,7 +21,7 @@ type RootProps = React.ComponentProps<'div'>
 export function Root(props: RootProps) {
   return (
     <div
-      className="flex h-11 items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex h-11 items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     ></div>
   )
