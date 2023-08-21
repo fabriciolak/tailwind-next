f4451713919c44dda6593e0f016192d3b5cbf4d7
Fabricio <fabriciolak12@gmail.com>
2023-08-21 14:28:36 -0300

    feat: button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Select } from '@/components/Form/Select'
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   const [currentTab, setCurrentTab] = React.useState<string>('my-details')
@@ -71,19 +72,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings-form"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings-form">
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -288,18 +282,12 @@ export default function Home() {
 
           {/* Buttons */}
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings-form">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,16 @@ const buttonVariants = tv({
     'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
     'active:opacity-80',
   ],
+
+  variants: {
+    variant: {
+      primary: 'bg-violet-600 text-white hover:bg-violet-700',
+      outline: 'border-zinc-300 border text-zinc-700 hover:bg-zinc-50',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
 })
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
@@ -16,11 +26,15 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, asChild = false, ...props }, ref) => {
+  ({ children, className, asChild = false, variant, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
 
     return (
-      <Comp ref={ref} className={buttonVariants({ className })} {...props}>
+      <Comp
+        ref={ref}
+        className={buttonVariants({ className, variant })}
+        {...props}
+      >
         {children}
       </Comp>
     )
